edcc3e27b49f987f0b7c51dd2f119a0274cef768
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-09 18:56:02 -0800

    movie carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import api from "./api/axiosConfig.js";
 import { useState, useEffect } from "react";
+import Layout from "./components/Layout.js";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.js";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -9,18 +12,22 @@ function App() {
       const response = await api.get("/api/v1/movies");
       console.log(response.data);
       setMovies(response.data);
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   };
 
-  useEffect(() =>{
+  useEffect(() => {
     getMovies();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
