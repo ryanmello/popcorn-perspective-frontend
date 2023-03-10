6d73b45d626173f4b5d04298543cb774957f0dd0
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-09 18:03:11 -0800

    connect frontend to database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import api from "./api/axiosConfig.js";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() =>{
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <header className="App-header">Hello</header>
     </div>
   );
 }
