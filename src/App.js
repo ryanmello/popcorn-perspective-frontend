dc7d4af80419b3440764e37f07acbd757c26a168
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-10 16:57:56 -0800

    header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Layout from "./components/Layout.js";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.js";
+import Header from "./components/header/Header.js";
 import './App.css'
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
