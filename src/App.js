d4382b3991ff9c033e7520bf6b289a12a42ab1a6
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-10 17:43:16 -0800

    watch movie trailer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout.js";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.js";
 import Header from "./components/header/Header.js";
+import Trailer from "./components/trailer/Trailer.js";
 import './App.css'
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
         </Route>
       </Routes>
     </div>
